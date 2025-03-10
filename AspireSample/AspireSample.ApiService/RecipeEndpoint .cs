aa7e1e9c0d82e29e49991e2d6eb3969652122db7
Alex van Wolferen <alexvanwolferen@gmail.com>
2025-03-10 17:08:45 +0100

    Update

---
+++ AspireSample/AspireSample.ApiService/RecipeEndpoint .cs
@@ -0,0 +1,39 @@
+﻿using AspireSample.ApiService.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AspireSample.ApiService
+{
+  public static class RecipeEndpoints
+  {
+    public static WebApplication MapRecipeEndpoints(this WebApplication app)
+    {
+      app.MapGet("/recipe/{id}", async (string id, FedDbContext context) =>
+      {
+        var sql = $"SELECT Id, Code, HouseNumber, City, StreetName FROM PostalCodes WHERE Id = '{id}'";
+        var result = context.Recipes.FromSqlRaw(sql).AsQueryable() is { } code
+            ? Results.Ok(code)
+            : Results.NotFound();
+
+        return result;
+      })
+      .WithName("GetRecipe");
+
+      app.MapGet("/recipes", async (string id, FedDbContext context) =>
+      {
+        var result = context.Recipes.Where(r => r.IsSecret == false).AsQueryable() is { } code
+            ? Results.Ok(code)
+            : Results.NotFound();
+
+        return result;
+      })
+      .WithName("GetRecipes");
+
+      return app;
+    }
+
+    record Code(string PostalCode, string City, string HouseNumber, string Street);
+  }
+}
--- AspireSample/AspireSample.AppHost/Program.cs
+++ AspireSample/AspireSample.AppHost/Program.cs
@@ -5,18 +5,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 var cache = builder.AddRedis("cache");
 
 var sqlserver = builder.AddSqlServer("sqlserver")
-  // Mount the init scripts directory into the container.
-  .WithBindMount("./sqlserverconfig", "/usr/config")
-  // Mount the SQL scripts directory into the container so that the init scripts run.
-  .WithBindMount("../AspireSample.ApiService/data/sqlserver", "/mnt/sql-init")
-  // Run the custom entrypoint script on startup.
-  //.WithEntrypoint("/usr/config/entrypoint.sh")
   // Configure the container to store data in a volume so that it persists across instances.
   .WithDataVolume()
   .WithLifetime(ContainerLifetime.Persistent);
 
 var db = sqlserver
-  .AddDatabase(name: "master");
+  .AddDatabase("master");
 
 var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice")
   .WithReference(db)
